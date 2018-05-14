d15be8a14239f1d2797642b3e3ae5a4338fbf384
Projet2A <projet2a@imac-cours-6.local>
2018-05-14 11:08:57 +0200

    version initiale

--- HideNSeek/ViewController.swift
+++ HideNSeek/ViewController.swift
@@ -12,22 +12,36 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    var hide : Bool = true
+    var nodeModel:SCNNode!
+    let nodeName = "shiba"
+    var objectPosition : SCNVector3!
+    var object : SCNNode!
+    
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var hideButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.antialiasingMode = .multisampling4X
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let modelScene = SCNScene(named:
+            "art.scnassets/shiba/shiba.dae")!
+        
+        nodeModel =  modelScene.rootNode.childNode(
+            withName: nodeName, recursively: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +49,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -51,17 +65,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
+     return node
+     }
+     */
+    func calculateDistance(from:SCNVector3,to:SCNVector3) -> Float{
+        let x = from.x - to.x
+        let y = from.y - to.y
+        return sqrtf( (x * x) + (y * y))
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -77,4 +96,105 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if hide {
+            let location = touches.first!.location(in: sceneView)
+            var hitTestOptions = [SCNHitTestOption: Any]()
+            hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+            let hitResults: [SCNHitTestResult]  =
+                sceneView.hitTest(location, options: hitTestOptions)
+            if let hit = hitResults.first {
+                if let node = getParent(hit.node) {
+                    node.removeFromParentNode()
+                    return
+                }
+            }
+            let hitResultsFeaturePoints: [ARHitTestResult] =
+                sceneView.hitTest(location, types: .featurePoint)
+            if let hit = hitResultsFeaturePoints.first {
+                // Get a transformation matrix with the euler angle of the camera
+                let rotate = simd_float4x4(SCNMatrix4MakeRotation(sceneView.session.currentFrame!.camera.eulerAngles.y, 0, 1, 0))
+                
+                // Combine both transformation matrices
+                let finalTransform = simd_mul(hit.worldTransform, rotate)
+                
+                // Use the resulting matrix to position the anchor
+                sceneView.session.add(anchor: ARAnchor(transform: finalTransform))
+                // sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+            }
+        }
+        else{
+            let location = touches.first!.location(in: sceneView)
+            var hitTestOptions = [SCNHitTestOption: Any]()
+            hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+            let hitResults: [SCNHitTestResult]  =
+                sceneView.hitTest(location, options: hitTestOptions)
+            if let hit = hitResults.first {
+                if let node = getParent(hit.node) {
+                    hide = true
+                    hideButton.isHidden = false
+                    node.removeFromParentNode()
+                    return
+                }
+            }
+        }
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if !hide {
+            guard let pointOfView = sceneView.pointOfView else { return }
+            let transform = pointOfView.transform
+            let orientation = SCNVector3(-transform.m31, -transform.m32, transform.m33)
+            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+            let currentPositionOfCamera = SCNVector3(orientation.x + location.x, orientation.y + location.y, orientation.z + location.z)
+            if objectPosition != nil {
+                let distance = calculateDistance(from: objectPosition, to: currentPositionOfCamera)
+                if distance < 1{
+                    object.isHidden = false
+                }else{
+                    object.isHidden = true
+                }
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if hide{
+            objectPosition = SCNVector3Make(anchor.transform.columns.3.x,anchor.transform.columns.3.y,anchor.transform.columns.3.z)
+            object = node
+        }
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func onButtonClick(_ sender: Any) {
+        if hide {
+            hide = false
+            hideButton.isHidden = true
+        }
+        else{
+            hide = true
+        }
+    }
+    
 }
