87ebc76aaaf76a550c742aaa538c7cefedb9990c
fanny ringler <fan96@hotmail.fr>
2018-05-17 12:03:27 +0200

    design classement

--- HideNSeek/VictoryViewController.swift
+++ HideNSeek/VictoryViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class VictoryViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var label: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = ""
         players.sort(by: {$0.score < $1.score})
         for player in players {
-            label.text?.append("\(player.name) \(player.score / 60) m \(player.score % 60) s ")
+            label.text?.append("\(player.name) \t \(player.score / 60) m \(player.score % 60) s \n")
         }
     }
 
